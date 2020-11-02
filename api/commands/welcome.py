b4da51584a5ffb6768c1a2f35d3227ac6d3bca4f
Alban Moreon <alban@MacBook-Pro-de-Alban.local>
2020-11-01 20:31:47 -0500

    Updated welcome page

--- api/commands/welcome.py
+++ api/commands/welcome.py
@@ -1,6 +1,14 @@
 import falcon
+import socket
+
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
+
 
 class Welcome(object):
 	def on_get(self, req, resp):
 		resp.status = falcon.HTTP_200
-		resp.body = ('Welcome to the Bambinito API\nPlease contact bambinito.dev@gmail.com for any questions/inquiries...')
+		resp.body = ("Welcome to the Bambinito API\n"\
+			"Please contact bambinito.dev@gmail.com for any questions/inquiries...\n\n"\
+			"Hostname: {}\n"\
+			"Ip: {}\n".format(hostname, ip))
